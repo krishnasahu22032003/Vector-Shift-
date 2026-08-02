87bb6029507b9aa5d05a206a04b15bc628da7489
krishna sahu <krishna.sahu.work@gmail.com>
2026-08-02 10:12:18 +0530

    llm config file add

---
+++ frontend/src/configs/llm.config.js
@@ -0,0 +1,39 @@
+export const inputConfig = {
+
+    title: "LLM" ,
+
+fields: [
+
+    ] ,
+
+handles: {
+
+        left: [
+
+            {
+
+                id: "system"
+
+            },
+
+            {
+
+                id: "prompt"
+
+            }
+
+        ],
+
+        right: [
+
+            {
+
+                id: "response"
+
+            }
+
+        ]
+
+    }
+
+}
