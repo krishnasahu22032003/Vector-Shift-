d9d86eb7b101de3ee4045556e94826dc466ebdf4
krishna sahu <krishna.sahu.work@gmail.com>
2026-08-02 19:27:55 +0530

    api config add

---
+++ frontend/src/configs/api.config.js
@@ -0,0 +1,48 @@
+export const apiConfig = {
+  title: "API Request",
+
+  description: "Calls an external API.",
+
+  size: {
+    width: 240,
+    height: 140,
+  },
+
+  fields: [
+    {
+      id: "url",
+      label: "URL",
+      type: "text",
+      defaultValue: "https://",
+    },
+    {
+      id: "method",
+      label: "Method",
+      type: "select",
+      defaultValue: "GET",
+      options: [
+        {
+          label: "GET",
+          value: "GET",
+        },
+        {
+          label: "POST",
+          value: "POST",
+        },
+      ],
+    },
+  ],
+
+  handles: [
+    {
+      id: "input",
+      type: "target",
+      position: "left",
+    },
+    {
+      id: "response",
+      type: "source",
+      position: "right",
+    },
+  ],
+};
